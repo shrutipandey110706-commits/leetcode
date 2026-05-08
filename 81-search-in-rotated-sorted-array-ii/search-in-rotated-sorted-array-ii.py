7a272004e3d84d65e899b0100b5b1071502eba6a
shrutipandey110706-commits <shrutipandey110706@gmail.com>
2026-05-08 22:33:43 +0530

    Time: 0 ms (100.00%) | Memory: 12.6 MB (71.58%) - LeetSync

---
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: bool
+        """
+        
+        for i in range(len(nums)) :
+            if nums[i]==target:
+                return True
+        return False    
+        
