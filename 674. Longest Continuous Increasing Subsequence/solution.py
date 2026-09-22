5d317ddaf52fd6bb6714c9d29ea8ba8c3e098c87
Shruti Pandey <shrutipandey110706@gmail.com>
2026-09-22 16:15:03 +0530

    Create solution.py

---
+++ 674. Longest Continuous Increasing Subsequence/solution.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def findLengthOfLCIS(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if not nums :
+            return 0
+
+        current =1
+        longest =1    
+
+        for i in range (1,len(nums)) :
+            if nums [i]>nums[i-1]:
+                current +=1 
+            else:
+                current =1  
+
+            longest = max(longest,current)
+        return longest                  
+        
